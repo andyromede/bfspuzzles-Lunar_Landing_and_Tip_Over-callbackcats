aebc3ac1cde81c5e5f990865ced8e4dd92efae41
me2737 <me2737@rit.edu>
2021-12-04 21:36:32 -0500

    Less basic tipover (almost complex) 12/4/21

--- src/puzzles/tipover/ptui/TipOverPTUI.java
+++ src/puzzles/tipover/ptui/TipOverPTUI.java
@@ -4,6 +4,8 @@ import puzzles.tipover.model.TipOverConfig;
 import puzzles.tipover.model.TipOverModel;
 import util.Observer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -44,10 +46,8 @@ public class TipOverPTUI implements Observer<TipOverModel, Object > {
                     this.model.hint();
                 } else if (fields[0].contains("show")) {
                     this.model.show();
-                } else if (fields[0].contains("help")) {
-                    this.model.help();
                 } else {
-                    System.err.println("This is not a valid input.");
+                    this.model.help();
                 }
             }
 
@@ -60,6 +60,49 @@ public class TipOverPTUI implements Observer<TipOverModel, Object > {
     }
     @Override
     public void update(TipOverModel tipOverModel, Object o) {
-
+        switch ((String) o) {
+            case "show" -> displayBoard(tipOverModel.returnConfig());
+            case "illegal" -> System.out.println("That move's illegal.");
+            case "towertopple" -> {
+                System.out.println("A tower has been tipped over.");
+                displayBoard(tipOverModel.returnConfig());
+            }
+            case "invaliddir" -> model.help();
+            case "win" -> System.out.println("YOU WIN!");
+            case "alreadywon" -> System.out.println("You arlready won!");
+        }
+    }
+    private void displayBoard(TipOverConfig board)
+    {
+        int[] tempLoc = board.getCurrCoords();
+        int[] tempGoal = board.getGoalCoords();
+        int[][] tempBoard = board.getBoard();
+        String s = "";
+        s += "\n";
+        s += "  " ;
+        for(int col = 0; col < tempBoard[0].length; col++){
+            s += "   " + col;
+        }
+        s += "\n";
+        s += "   " ;
+        for(int col = 0; col < tempBoard[0].length; col++){
+            s += "----";
+        }
+        for (int i = 0; i < tempBoard.length; i++)
+        {
+            s += "\n" + i + " |";
+            for (int j = 0; j < tempBoard[0].length; j++) {
+                if (tempBoard[i][j] == 0)
+                    s += " _";
+                else if (tempLoc[0] == i && tempLoc[1] == j)
+                    s += "!" + tempBoard[i][j];
+                else if (tempGoal[0] == i && tempGoal[1] == j)
+                    s += "*" + tempBoard[i][j];
+                else
+                    s += " " + tempBoard[i][j];
+                s += "  ";
+            }
+        }
+        System.out.println(s);
     }
 }
